f0b6848a26d640a5451e051c4bb2285887e5f7c1
Ajaya <ajaya.ramachandran@gmail.com>
2024-07-07 15:04:05 -0400

    removed tkinter

--- inout.py
+++ inout.py
@@ -1,11 +1,12 @@
-import tkinter as tk
-from tkinter import filedialog
+#import tkinter as tk
+#from tkinter import filedialog
 import subprocess
 
+'''
 def importFile():
     filename = filedialog.askopenfilename()
     print('Selected:', filename)
     return filename
-
+'''
 def showOutput(filename):
     subprocess.Popen(["notepad.exe", filename], shell=True)
