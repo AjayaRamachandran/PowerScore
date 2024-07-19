612a977c8d79420acc9dab63727afbc4bebb1576
AjayaRamachandran <ajaya.ramachandran@gmail.com>
2024-07-19 03:10:40 -0400

    added bottom bar

--- api/dashboard.py
+++ api/dashboard.py
@@ -5,6 +5,62 @@ ending = '''
                 <div class="gap">
             <div class="gap"></div>
         </div>
+                <div class="big-gap">
+            <div class="big-gap"></div>
+        </div>
+        <div class="big-gap">
+            <div class="big-gap"></div>
+        </div>
+        <div class="big-gap">
+            <div class="big-gap"></div>
+        </div>
+        <div class="big-gap">
+            <div class="big-gap"></div>
+        </div>
+        <div class="big-gap">
+            <div class="big-gap"></div>
+        </div>
+        <div class="big-gap">
+            <div class="big-gap"></div>
+        </div>
+        <div class="bottom">
+            © Copyright 2024 by Ajaya Ramachandran, Team 8568A 23-24
+            <div class="bottom-bottom">
+                <div class="v-gap">
+                    <div class="v-gap"></div>
+                </div>
+                <div class="v-gap">
+                    <div class="v-gap"></div>
+                </div>
+                <div class="bottom-column">
+                    <div class="bottom-column-item"><a target="_blank" href="https://forms.gle/uv3X3gEELeUg8D2L6">Improve This Site</a></div>
+                    <div class="bottom-column-item"><a target="_blank" href="mailto:na8568a@gmail.com">Contact Us</a></div>
+                    <div class="bottom-column-item"><a target="_blank" href="https://github.com/AjayaRamachandran">Support Me on GitHub</a></div>
+                    <div class="bottom-column-item"><a target="_blank" href="https://www.dropbox.com/scl/fi/kug4ka2zejyvz9yduh5i3/Credits-and-Acknowledgements.pdf?rlkey=gn6tuqgpfump219muz10rduri&st=amh43ydv&dl=0">Credits</a></div>
+                </div>
+                <div class="bigger-gap">
+                    <div class="bigger-gap"></div>
+                </div>
+                <div class="bigger-gap">
+                    <div class="bigger-gap"></div>
+                </div>
+                <div class="bigger-gap">
+                    <div class="bigger-gap"></div>
+                </div>
+                <div class="bigger-gap">
+                    <div class="bigger-gap"></div>
+                </div>
+                <div class="bigger-gap">
+                    <div class="bigger-gap"></div>
+                </div>
+                <div class="bigger-gap">
+                    <div class="bigger-gap"></div>
+                </div>
+                <div class="bottom-column">
+                    <div class="bottom-column-item"><a href="https://vercel.com/docs">Hosted by Vercel</div>
+                </div>
+            </div>
+        </div>
         </div>
 </body>
 </html>'''
