95ae08b0a588bce4f5dc857f6611169e3300e410
Ajaya <ajaya.ramachandran@gmail.com>
2024-08-01 04:42:15 -0400

    completed added back reload.js

--- api/static/js/dropdown-season.js
+++ api/static/js/dropdown-season.js
@@ -4,14 +4,3 @@ const formElement = document.getElementById('myForm');
 selectElement.addEventListener('change', () => {
     formElement.submit();
 });
-
-function reloadCSS() {
-    var links = document.getElementsByTagName("link");
-    for (var i = 0; i < links.length; i++) {
-        var link = links[i];
-        if (link.rel === "stylesheet" && link.id != "freeze") {
-            link.href = link.href.split("?")[0] + "?reload=" + new Date().getTime();
-        }
-    }
-}
-reloadCSS();
