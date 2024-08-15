8064a04f9ec8ea12ffd409fc81fc0895729231d4
AjayaRamachandran <ajaya.ramachandran@gmail.com>
2024-08-15 01:15:53 -0400

    test

--- api/app.py
+++ api/app.py
@@ -216,12 +216,12 @@ def handle_competitions():
             user_agent = request.headers.get('User-Agent').lower()
             if 'iphone' in user_agent or 'android' in user_agent or mobile == "Y":
                 homeButton = "Home"
-                htmlFile = render_template("comp.html", debug = debug, home = home, homeButton = homeButton, mobile = [url_for('static', filename='/css/mainstyle-mobile.css'),
+                htmlFile = render_template("comp.html", debug = debug, home = home, homeButton = homeButton, name = result[0], mobile = [url_for('static', filename='/css/mainstyle-mobile.css'),
                                                                                                         url_for('static', filename='/css/bottom-mobile.css'),
                                                                                                         url_for('static', filename='/css/comps-mobile.css')]) + pageGen.generateFrom(result, query, division, "")
             else:
                 homeButton = "Back to Home"
-                htmlFile = render_template("comp.html", debug = debug, home = home, homeButton = homeButton, mobile = [url_for('static', filename='/css/mainstyle.css'),
+                htmlFile = render_template("comp.html", debug = debug, home = home, homeButton = homeButton, name = result[0], mobile = [url_for('static', filename='/css/mainstyle.css'),
                                                                                                         url_for('static', filename='/css/bottom.css'),
                                                                                                         url_for('static', filename='/css/comps.css')]) + pageGen.generateFrom(result, query, division, "")
             return htmlFile
