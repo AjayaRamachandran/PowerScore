30fddb9a6a77961de607dc8d720de60662349bff
AjayaRamachandran <ajaya.ramachandran@gmail.com>
2024-12-28 22:57:33 -0500

    added full upgraded mobile UI suport

--- api/pageGen.py
+++ api/pageGen.py
@@ -59,7 +59,7 @@ def generateFrom(info, sku, division = "1", excelFile = None):
             <form id="myForm" action="/competitions" method="GET">
                 <input type="hidden" id="sku" name="query" value="{sku}">
                 <label for="division">Division</label>
-                <select name="division" id="divs" class="dropdown" visual="bg">
+                <select name="division" id="divs" class="dropdown fit" visual="bg">
 '''
     
     for divID, divName in enumerate(divisions):
@@ -78,7 +78,12 @@ def generateFrom(info, sku, division = "1", excelFile = None):
         <div class="gap-2">
             <div class="gap-2"></div>
         </div>
-        <form action="/download" method="GET" class="download-button" visual="tint"><button visual="tint" type="submit" class="download-text">Download XLSX</button></form>
+        <form action="/download" method="GET" class="download-button" visual="tint"><button visual="tint" type="submit" class="download-text">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
+</svg>
+        </button></form>
     </div>
     <div class="gap-2">
         <div class="gap-2"></div>
@@ -86,7 +91,7 @@ def generateFrom(info, sku, division = "1", excelFile = None):
         ''' + '''
     <div class="body-container-top" visual="tint">
         <div class="body-contained-5">Rank</div>
-        <div class="body-contained-5">Team Name</div>
+        <div class="body-contained-5">Team</div>
         <div class="body-contained-5">Powerscore</div>
         <div class="body-contained-5">OPS</div>
         <div class="body-contained-5">DPS</div>
@@ -97,7 +102,7 @@ def generateFrom(info, sku, division = "1", excelFile = None):
         bodyList = bodyList + f'''
     <div class="body-container1">
         <div class="body-contained-4"><b>{str(team + 1) + ". "}</b></div>
-        <div class="body-contained-2"><b><a href={"http://localhost:5000" if (debug == "Y") else "https://powerscore.vercel.app"}/teams?query={powerscores[team][0]} style="text-decoration: underline;">{powerscores[team][0]}</a></b></div>
+        <div class="body-contained-2"><b><a class="backlink" href={"http://localhost:5000" if (debug == "Y") else "https://powerscore.vercel.app"}/teams?query={powerscores[team][0]} ">{powerscores[team][0]}</a></b></div>
         <div class="body-contained-1">{powerscores[team][1]}</div>
         <div class="body-contained-3">{offensive[team][1]}</div>
         <div class="body-contained-4">{defensive[team][1]}</div>
